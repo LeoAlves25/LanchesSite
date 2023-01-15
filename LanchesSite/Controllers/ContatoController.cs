036369d29c02a5b43198d430e437cc5b140109f7
LeoAlves25 <leo_alves25@yahoo.com.br>
2023-01-15 10:43:55 -0300

    TagHelpers

---
+++ LanchesSite/Controllers/ContatoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesSite.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
