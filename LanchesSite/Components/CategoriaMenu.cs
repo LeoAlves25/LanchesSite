46180f5eefeb78ef47c0e71eda17914fc1bdd949
LeoAlves25 <leo_alves25@yahoo.com.br>
2023-01-15 17:02:58 -0300

    Rotas, View Component categoria e lanches

---
+++ LanchesSite/Components/CategoriaMenu.cs
@@ -0,0 +1,21 @@
+﻿using LanchesSite.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesSite.Components
+{
+    public class CategoriaMenu : ViewComponent
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public CategoriaMenu(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categoria = _categoriaRepository.Categorias.OrderBy(c => c.CategoriaNome);
+            return View(categoria);
+        }
+    }
+}
