25dbf8313c9ff81989293b520d2f9fb9cad3373e
LeoAlves25 <leo_alves25@yahoo.com.br>
2023-01-24 20:11:56 -0300

    Opções para upload e consulta de imagens

---
+++ LanchesSite/Areas/Admin/Controllers/AdminImagensController.cs
@@ -0,0 +1,26 @@
+﻿using LanchesSite.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace LanchesSite.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class AdminImagensController : Controller
+    {
+        private readonly ConfigurationImagens _myConfig;
+        private readonly IWebHostEnvironment _hostingEnviroment;
+
+        public AdminImagensController(IOptions<ConfigurationImagens> myConfig, IWebHostEnvironment hostingEnviroment)
+        {
+            _myConfig = myConfig.Value;
+            _hostingEnviroment = hostingEnviroment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
