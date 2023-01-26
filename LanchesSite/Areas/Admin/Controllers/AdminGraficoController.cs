033f59bd2c39c406fa6fc5919bdd022448435082
LeoAlves25 <leo_alves25@yahoo.com.br>
2023-01-25 21:05:35 -0300

    View Gráfico Anual

--- LanchesSite/Areas/Admin/Controllers/AdminGraficoController.cs
+++ LanchesSite/Areas/Admin/Controllers/AdminGraficoController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesSite.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class AdminGraficoController : Controller
     {
         private readonly GraficoVendasService _graficoVendas;
